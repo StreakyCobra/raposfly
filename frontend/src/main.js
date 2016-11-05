5d5ce21e648bba8eb0050adc815f622307573a34
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-11-05 23:10:24 +0100

    Add jquery and bootstrap js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App'
+
+// jquery
+var $ = require('jquery')
+window.jQuery = window.$ = $
+
+// bootstrap
+require('bootstrap')
 import 'bootstrap/dist/css/bootstrap.css'
 
 /* eslint-disable no-new */
