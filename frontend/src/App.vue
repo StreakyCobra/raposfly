244e5a50030621dddacea368fd27979a82bc5b5b
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-11-06 15:52:12 +0100

    Use Monserrat font

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,7 +19,22 @@ export default {
 </script>
 
 <style>
+@font-face {
+  font-family: 'Montserrat';
+  src: url('/static/fonts/Montserrat-Black.otf');
+  src: url('/static/fonts/Montserrat-Bold.otf');
+  src: url('/static/fonts/Montserrat-ExtraBold.otf');
+  src: url('/static/fonts/Montserrat-ExtraLight.otf');
+  src: url('/static/fonts/Montserrat-Light.otf');
+  src: url('/static/fonts/Montserrat-Medium.otf');
+  src: url('/static/fonts/Montserrat-Regular.otf');
+  src: url('/static/fonts/Montserrat-SemiBold.otf');
+  src: url('/static/fonts/Montserrat-Thin.otf');
+  src: url('/static/fonts/Montserrat-UltraLight.otf');
+  src: local('Monserrat') format('truetype');
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', Helvetica, Arial, sans-serif;
 }
 </style>
