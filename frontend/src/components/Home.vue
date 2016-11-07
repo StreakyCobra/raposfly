1bf6aff7e5b3a76700ca19717a08a5f353f11978
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-11-07 16:39:01 +0100

    Add links to home components

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="container">
-        <h1 class="page-header">Home</h1>
+        <h1 class="page-header">Welcome to raposfly</h1>
+        <p>
+            You can directly access the shop:<br />
+            <router-link to="/shop" tag="a" class="btn btn-info" style="width: 150px;">Shop</router-link>
+        </p>
+        <p>
+            Or see the history from:<br />
+            <router-link to="/history" tag="a" class="btn btn-info" style="width: 150px;">History</router-link>
+        </p>
     </div>
 </template>
 
