4cd3204735d3efb54a2b0b1090c55b03e2c5e3e2
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-11-07 22:52:17 +0100

    Better languages support through assets

--- frontend/src/language.js
+++ frontend/src/language.js
@@ -1,30 +1,17 @@
 import Vue from 'vue'
+import en from './assets/i18n/en.json'
+import fr from './assets/i18n/fr.json'
 
-// HACK: Change the lang first so it detect a change when setting english…
-Vue.config.lang = 'WRONG?'
+var locales = {
+    en: en,
+    fr: fr
+}
 
-// isomorphic-fetch
-var fetch = require('isomorphic-fetch')
+Object.keys(locales).forEach(function (lang) {
+    Vue.locale(lang, locales[lang])
+})
 
 export function setLang (lang) {
-    Vue.locale(lang, function () {
-        return fetch('/static/i18n/' + lang + '.json', {
-            method: 'get',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        }).then(function (res) {
-            return res.json()
-        }).then(function (json) {
-            if (Object.keys(json).length === 0) {
-                return Promise.reject(new Error('locale empty !!'))
-            } else {
-                return Promise.resolve(json)
-            }
-        })
-    }, function () {
-        Vue.config.lang = lang
-        Vue.config.fallbackLang = 'en'
-    })
+    Vue.locale(lang, locales[lang])
+    Vue.config.lang = lang
 }
