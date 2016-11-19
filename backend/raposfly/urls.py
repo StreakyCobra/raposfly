cb9fd6993978e97a3a05190b23511890daa44f45
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-11-19 12:09:31 +0100

    Fix warning about URLS

--- backend/raposfly/urls.py
+++ backend/raposfly/urls.py
@@ -21,9 +21,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('rest_framework_docs.urls')),
-    url(r'^admin/', admin.site.urls),
     url(r'^api/shop/', include('shop.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('rest_framework_docs.urls')),
 ]
 
 if settings.DEBUG:
