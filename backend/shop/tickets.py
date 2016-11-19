de704fad105e04e639d96e0da2b7012582a60936
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-11-19 15:10:07 +0100

    Improve ticket for zinal badminton

--- backend/shop/tickets.py
+++ backend/shop/tickets.py
@@ -2,36 +2,29 @@
 """Tickets printing for the application."""
 
 
-CURRENT = 0
 WIDTH = 42
 
 
 def print_headline(printer):
     """Print the headline."""
     printer.set('center', 'A', 'U')
-    printer.text("SOIREE DU BADMINTON 2016\n\n")
+    printer.text("SOIREE DU BADMINTON ZINAL 2016\n\n")
 
 
 def print_item(printer, item):
     """Print an item."""
     spacing = WIDTH - 3 - len(item.name) - 5 - 4
-    printer.text('1x {}{}{: >5.2f} CHF\n\n'.format(item.name,
+    printer.text('1x {}{}{: >5.2f} CHF\n'.format(item.name,
                                                    ' ' * spacing,
                                                    item.price))
 
 def print_ticket(printer, item):
     """Print an item ticket."""
-    # Current ticket
-    global CURRENT
-    CURRENT += 1
     # Print headline
     print_headline(printer)
     # Print the item
-    printer.set('left', 'A', 'B')
+    printer.set('left', 'A', 'B', 1, 2)
     print_item(printer, item)
-    # Print ticket number
-    printer.set('center')
-    printer.text('#{:04d}'.format(CURRENT))
     # Cut the ticket
     printer.cut()
 
@@ -40,6 +33,11 @@ def print_total(printer, items, total):
     """Print the sum ticket."""
     # Print headline
     print_headline(printer)
+    printer.set('center', 'A', 'B')
+    printer.text('N\'EST PAS UN BON A ECHANGER.\n')
+    # Print header
+    printer.set('left', 'A', 'U')
+    printer.text('Quittance:\n\n')
     # Print each item
     printer.set()
     for item in items:
