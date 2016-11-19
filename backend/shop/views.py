58e61f9ba11791f91a8fabb800c386c674e8ee93
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-11-19 15:42:05 +0100

    Set code for TM-T20

--- backend/shop/views.py
+++ backend/shop/views.py
@@ -78,7 +78,7 @@ class PurchaseItems(APIView):
         # ------------------------------------------------------------------- #
 
         try:
-            printer = Usb(0x04b8, 0x0e02)
+            printer = Usb(0x04b8, 0x0e15)
         except USBNotFoundError:
             return Response({'status': 'Impossible to connect to the '
                                        'printer.'},
