ae00de7179dbbe9f3b7500bda75f757bcf45f8eb
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-11-22 11:17:22 +0100

    Fix delete button request and style in history

--- frontend/src/components/History.vue
+++ frontend/src/components/History.vue
@@ -3,9 +3,9 @@
         <h1 class="page-header">{{ $t('History') }}</h1>
         <template v-for="purchase in purchases">
             <div class="panel panel-info">
-                <div class="panel-heading">
+                <div class="panel-heading clearfix">
                     <div class="btn-group pull-right">
-                        <a class="btn btn-danger btn-sm" @click="remove(purchase)">{{ $t('Delete') }}</a>
+                        <a class="btn btn-danger btn-xs" @click="remove(purchase)">{{ $t('Delete') }}</a>
                     </div>
                     {{ format_date(purchase.date) }}
                 </div>
@@ -47,7 +47,7 @@
              })
          },
          remove: function (purchase) {
-             this.$http.delete('shop/purchase/' + purchase.id + '/').then((response) => {
+             this.$http.delete('shop/purchases/' + purchase.id + '/').then((response) => {
                  this.update()
              }, (response) => {
                  this.$emit('error', 'Impossible to delete purchase')
@@ -62,11 +62,7 @@
 </script>
 
 <style scoped>
- .purchase {
-     background-repeat:repeat-x;
-     border-radius:4px;
-     box-shadow:rgba(255, 255, 255, 0.14902) 0 1px 0 inset, rgba(0, 0, 0, 0.0745098) 0 1px 5px;
-     padding:1em;
-     margin-bottom: 1em;
+ .panel-heading {
+     line-height: 22px;
  }
 </style>
