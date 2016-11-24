94514cecfdff44c0bbe7d16d0a1e95f341f16f96
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-11-24 23:46:06 +0100

    Use config 'EVENT' in frontend \o/

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <h1 class="page-header">{{ $t('Welcome to raposfly') }}</h1>
+        <h1 class="page-header">{{ $t(event) }}</h1>
         <p>
             {{ $t('You can directly access the shop') }}:<br />
             <router-link to="/shop" tag="a" class="btn btn-info" style="width: 150px;">{{ $t('Shop') }}</router-link>
@@ -18,6 +18,18 @@
 
 <script>
  export default {
-     name: 'home'
+     name: 'home',
+     data: function () {
+         return {
+             event: 'Welcome to raposfly'
+         }
+     },
+     mounted: function () {
+         this.$http.get('shop/config/').then((response) => {
+             this.event = response.body['EVENT']
+         }, (response) => {
+             this.$bus.$emit('error', 'Impossible to connect to the database')
+         })
+     }
  }
 </script>
