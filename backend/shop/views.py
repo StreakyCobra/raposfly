baee4ddc323933c86b306cf961be5e2dc3a060ec
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-12-04 21:09:46 +0100

    Disable printing

--- backend/shop/views.py
+++ backend/shop/views.py
@@ -85,29 +85,31 @@ class BuyView(APIView):
         # ------------------------------------------------------------------- #
 
         try:
-            printer = Usb(0x04b8, 0x0e15)
+            pass
+            # printer = Usb(0x04b8, 0x0e15)
         except USBNotFoundError:
             return Response({'status': 'Impossible to connect to the '
                                        'printer.'},
                             status=400)
 
         try:
-            total=0
-            for (item, quantity) in items:
-                if item.individual_ticket:
-                    for _ in range(quantity):
-                        print_ticket(printer, item)
-                total += item.price * quantity
-            if receipt:
-                print_total(printer, items, total)
-            if config.VENDOR_RECEIPT:
-                print_total(printer, items, total)
+            pass
+            # total=0
+            # for (item, quantity) in items:
+            #     if item.individual_ticket:
+            #         for _ in range(quantity):
+            #             print_ticket(printer, item)
+            #     total += item.price * quantity
+            # if receipt:
+            #     print_total(printer, items, total)
+            # if config.VENDOR_RECEIPT:
+            #     print_total(printer, items, total)
         except Error:
             return Response({'status': 'Impossible to print the tickets'},
                             status=400)
 
         # ------------------------------------------------------------------- #
-        # Store the purchasñe                                                 #
+        # Store the purchase                                                 #
         # ------------------------------------------------------------------- #
 
         purchase = Purchase(date=timezone.now())
