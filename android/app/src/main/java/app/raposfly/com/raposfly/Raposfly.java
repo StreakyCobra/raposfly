13bb79b894e6e490ea4779ba174efd824cf445c9
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-12-04 13:37:23 +0100

    Add the android application webview

---
+++ android/app/src/main/java/app/raposfly/com/raposfly/Raposfly.java
@@ -0,0 +1,72 @@
+package app.raposfly.com.raposfly;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Raposfly extends AppCompatActivity {
+
+    private WebView loadingWebView;
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_raposfly);
+
+        loadingWebView = (WebView) findViewById(R.id.activity_loading_webview);
+        loadingWebView.loadUrl("file:///android_asset/loading.html");
+        loadingWebView.setVisibility(View.VISIBLE);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        mWebView.setWebViewClient(new WebViewClient() {
+            boolean loadingFinished = true;
+            boolean redirect = false;
+
+            @Override
+            public boolean shouldOverrideUrlLoading(
+                    WebView view, WebResourceRequest request) {
+                if (!loadingFinished) {
+                    redirect = true;
+                }
+
+                loadingFinished = false;
+                mWebView.loadUrl(request.getUrl().toString());
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(
+                    WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                loadingFinished = false;
+                loadingWebView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                if(!redirect){
+                    loadingFinished = true;
+                }
+
+                if(loadingFinished && !redirect){
+                    loadingWebView.setVisibility(View.INVISIBLE);
+                } else{
+                    redirect = false;
+                }
+            }
+        });
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.loadUrl("http://raposfly.shop");
+    }
+}
