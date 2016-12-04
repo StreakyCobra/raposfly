e7f5a150cabdc43ce9016aa5ab57ce58dc9f20ec
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-12-04 16:32:44 +0100

    Move webpack's jquery plugin to base instead of dev

--- frontend/build/webpack.dev.conf.js
+++ frontend/build/webpack.dev.conf.js
@@ -29,11 +29,6 @@ module.exports = merge(baseWebpackConfig, {
       filename: 'index.html',
       template: 'index.html',
       inject: true
-    }),
-    new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        "window.jQuery": 'jquery'
     })
   ]
 })
