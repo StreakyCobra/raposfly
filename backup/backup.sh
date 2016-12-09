1bfe0bb3f017d45787274cf2b4b9dd9232e48325
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-12-09 20:51:48 +0100

    Run cron backup job as root each minute

--- backup/backup.sh
+++ backup/backup.sh
@@ -18,6 +18,9 @@ F_NAME=db.sqlite3
 # To name:
 T_NAME=$(date +%Y-%m-%d"_"%H-%M-%S).sqlite3
 
+# Create the backup directory if it doesn't exists
+mkdir -p "${TO}"
+
 # Do the copy
 cp -a "${FROM}/${F_NAME}" "${TO}/${T_NAME}" 2>>${LOG} && echo "Backup saved to ${T_NAME}" >> ${LOG}
 
