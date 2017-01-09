3ed04beb368bc054b0d7a75e8792c611b40a1e89
Fabien Dubosson <fabien.dubosson@gmail.com>
2017-01-09 22:55:17 +0100

    Add Numpad for giving back money
    Close #42

--- frontend/src/components/Cart.vue
+++ frontend/src/components/Cart.vue
@@ -1,28 +1,42 @@
 <template>
     <div class="cart">
         <h1 class="page-header">{{ $t('Cart') }}</h1>
-        <div class="controls">
-            <p class="total">Total: <span class="amount">{{ total.toFormat(2) }} CHF</span></p>
-            <div class="buttons btn-group">
-                <button class="btn btn-lg btn-success" @click="purchaseCart">{{ $t('Purchase') }}</button>
-                <button class="btn btn-lg btn-danger" @click="discardCart">{{ $t('Discard') }}</button>
+        <div v-if="cart.length > 0 || last_total.eq(0)">
+            <div class="controls">
+                <p class="total">Total: <span class="amount">{{ total.toFormat(2) }} CHF</span></p>
+                <div class="buttons btn-group">
+                    <button class="btn btn-lg btn-success" @click="purchase">{{ $t('Purchase') }}</button>
+                    <button class="btn btn-lg btn-danger" @click="discard">{{ $t('Discard') }}</button>
+                </div>
             </div>
+            <checkbox :label="$t('Print client receipt')" :checked="receipt" @change="toggleReceipt"></checkbox>
+            <hr />
+            <template
+                v-for="entry in cart">
+                <item display_style=small
+                      :item="entry.item"
+                      :quantity="entry.quantity"
+                      @clicked="removeFromCart(entry.item)"/>
+            </template>
         </div>
-        <checkbox :label="$t('Print client receipt')" :checked="receipt" @change="toggleReceipt"></checkbox>
+        <div v-else-if="last_total.gt(0)">
+            <div class="total alert alert-warning">
+                Paid: <span class="amount">{{ given.toFormat(2) }} CHF</span>
+                <br />
+                To pay: <span class="amount underline"><i class="fa fa-minus" style="font-size: 0.8em;"></i>&nbsp;{{ last_total.toFormat(2) }} CHF</span>
+            </div>
+            <div :class="'toReturn alert alert-' + (to_return.gt(0) || given.eq(0) || given.eq(last_total) ? 'success' : 'danger')" @click="exitNumpad"><i class="fa fa-sign-in"></i>&nbsp;{{ to_return.toFormat(2) }} CHF</div>
+            <hr />
+        <numpad ref="numpad" v-model="given"></numpad>
         <hr />
-        <template
-            v-for="entry in cart">
-            <item display_style=small
-                  :item="entry.item"
-                  :quantity="entry.quantity"
-                  @clicked="removeFromCart(entry.item)"/>
-        </template>
+    </div>
     </div>
 </template>
 
 <script>
  import Item from './Item'
  import Checkbox from './Checkbox'
+ import Numpad from './Numpad'
  import { mapActions, mapGetters } from 'vuex'
  import BigNumber from '../math.js'
 
@@ -30,7 +44,14 @@
      name: 'cart',
      components: {
          Checkbox,
-         Item
+         Item,
+         Numpad
+     },
+     data: function () {
+         return {
+             last_total: new BigNumber(0),
+             given: new BigNumber(0)
+         }
      },
      computed: {
          total: function () {
@@ -41,14 +62,33 @@
          ...mapGetters([
              'cart',
              'receipt'
-         ])
+         ]),
+         to_return: function () {
+             return this.given.gt(this.last_total) && this.given.minus(this.last_total) || new BigNumber(0)
+         }
      },
-     methods: mapActions([
-         'discardCart',
-         'purchaseCart',
-         'removeFromCart',
-         'toggleReceipt'
-     ])
+     methods: {
+         purchase: function () {
+             this.last_total = this.total
+             this.given = new BigNumber(0)
+             this.purchaseCart()
+         },
+         discard: function () {
+             this.last_total = new BigNumber(0)
+             this.given = new BigNumber(0)
+             this.discardCart()
+         },
+         exitNumpad: function () {
+             this.last_total = new BigNumber(0)
+             this.given = new BigNumber(0)
+         },
+         ...mapActions([
+             'discardCart',
+             'purchaseCart',
+             'removeFromCart',
+             'toggleReceipt'
+         ])
+     }
  }
 </script>
 
@@ -71,5 +111,20 @@
 
  .amount {
      float: right;
+ }
+
+ .padValues {
+     font-size: 1.2em;
+     font-weight: bold;
+ }
+
+ .toReturn {
+     font-size: 2em;
+     text-align: center;
+     cursor: pointer;
+ }
+
+ .underline {
+     border-bottom: solid 1px black;
  }
 </style>
---
+++ frontend/src/components/Numpad.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="numpad">
+        <div class="btn btn-info button" @click="digit(1)">
+            1
+        </div>
+        <div class="btn btn-info button" @click="digit(2)">
+            2
+        </div>
+        <div class="btn btn-info button" @click="digit(3)">
+            3
+        </div>
+        <div class="btn btn-info button" @click="digit(4)">
+            4
+        </div>
+        <div class="btn btn-info button" @click="digit(5)">
+            5
+        </div>
+        <div class="btn btn-info button" @click="digit(6)">
+            6
+        </div>
+        <div class="btn btn-info button" @click="digit(7)">
+            7
+        </div>
+        <div class="btn btn-info button" @click="digit(8)">
+            8
+        </div>
+        <div class="btn btn-info button" @click="digit(9)">
+            9
+        </div>
+        <div class="btn btn-danger button" @click="erase">
+            <i class="fa fa-remove"></i>
+        </div>
+        <div class="btn btn-info button" @click="digit(0)">
+            0
+        </div>
+        <div class="btn btn-success button" @click="dot">
+            •
+        </div>
+    </div>
+</template>
+
+<script>
+ import BigNumber from '../math.js'
+
+ export default {
+     name: 'numpad',
+     data: function () {
+         return {
+             value: ''
+         }
+     },
+     methods: {
+         digit: function (val) {
+             this.value += val
+             this.input()
+         },
+         erase: function () {
+             this.value = ''
+             this.input()
+         },
+         dot: function () {
+             if (this.value.indexOf('.') === -1) {
+                 this.value += '.'
+                 this.input()
+             }
+         },
+         input: function () {
+             this.$emit('input', new BigNumber(this.value || 0))
+         }
+     }
+ }
+</script>
+
+<style scoped>
+ .number {
+     display: flex;
+     flex-wrap: wrap;
+ }
+
+ .button {
+     display: inline-flex;
+     flex: 1 1 33%;
+     width: 32%;
+     margin: 0px 0px 4px 0px;
+     justify-content: center;
+     align-items: center;
+     height: 50px;
+     cursor: pointer;
+ }
+
+</style>
